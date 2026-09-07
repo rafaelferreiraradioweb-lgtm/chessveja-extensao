d47167716e409bc881b567a6b465deac232ef254
rafaelferreiraradioweb-lgtm <rafaelferreiraradioweb@gmail.com>
2026-09-07 19:37:55 -0300

    Fix comment formatting in background.js

--- background.js
+++ background.js
@@ -1,2 +1,2 @@
-// Permite que o painel lateral abra ao clicar no ícone da extensão
+// Permite que o Painel Lateral abra ao clicar no ícone da extensão
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch((error) => console.error(error));
